a63f88fde30244ab80e57fd7a078c8595c965e01
Andrea Brown <andreabrown7286@gmail.com>
2018-08-02 21:50:45 -0500

    Added links to Home page Created user signup form

--- src/main/java/org/launchcode/happycamper/controllers/CamperController.java
+++ src/main/java/org/launchcode/happycamper/controllers/CamperController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "camper")
+@RequestMapping("camper")
 public class CamperController {
 
     @RequestMapping(value = "")
@@ -14,7 +14,8 @@ public class CamperController {
 
         model.addAttribute("title", "HappyCamper");
         return "camper/index";
-        
     }
+
+
 
 }
--- src/main/java/org/launchcode/happycamper/controllers/UserController.java
+++ src/main/java/org/launchcode/happycamper/controllers/UserController.java
@@ -1,31 +1,37 @@
-//package org.launchcode.happycamper.controllers;
-//
-//import org.launchcode.happycamper.User;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.Errors;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//
-//
-//@Controller
-//@RequestMapping(value = "user")
-//public class UserController {
-//
-//    @RequestMapping(value = "add", method = RequestMethod.GET)
-//    public String add(Model model) {
-//        model.addAttribute("title", "HappyCamper User SignUp");
-//
-//        return "user/add";
-//    }
-//
-//    @RequestMapping(value = "add", method = RequestMethod.POST)
-//    public String processadd(Model model, @ModelAttribute User user, Errors errors, String verify) {
-//        if (errors.hasErrors()) {
-//            model.addAttribute(user);
-//        }
-//
-//        return "camper/index";
-//    }
-//}
+package org.launchcode.happycamper.controllers;
+
+import org.launchcode.happycamper.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping(value = "user")
+public class UserController {
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String adduser(Model model) {
+        model.addAttribute("title", "HappyCamper User SignUp");
+        model.addAttribute(new User());
+        return "user/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processadduser(Model model, @ModelAttribute User user, Errors errors) {
+        if (errors.hasErrors()){
+            model.addAttribute("title", "HappyCamper User SignUp");
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            model.addAttribute("password", user.getPassword());
+            model.addAttribute("message", "Passwords do not match!");
+            return "user/add";
+        }else{
+        model.addAttribute("user", user);
+        return "user/index";
+        }
+    }
+}
